11f1b06a0ca571151f3c38914b55d6eabd9f703a
Brad Dunlap <braddunlapdpt@gmail.com>
2023-01-14 14:10:51 -0500

    create NightWriter class that is testable

--- 1mod/projects/night_writer/spec/night_writer_spec.rb
+++ 1mod/projects/night_writer/spec/night_writer_spec.rb
@@ -1,1 +1,18 @@
+require_relative 'spec_helper'
 
+RSpec.describe NightWriter do
+  let(:night_writer) {NightWriter.new}
+  before do
+    night_writer.reader = './message.txt'
+    night_writer.writer = './braille.txt'
+  end
+  it 'exists' do
+    expect(night_writer).to be_a(NightWriter)
+  end
+
+  it 'has attributes' do
+    night_writer.run
+    expect(night_writer.reader).to eq('./message.txt')
+    expect(night_writer.writer).to eq('./braille.txt')
+  end
+end
--- 1mod/projects/night_writer/spec/spec_helper.rb
+++ 1mod/projects/night_writer/spec/spec_helper.rb
@@ -1,5 +1,4 @@
 require 'simplecov'
 SimpleCov.start
-require 'pry'
-require 'rspec'
 require './lib/night_writer'
+
