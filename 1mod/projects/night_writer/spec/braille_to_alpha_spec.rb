7514177e2b764fe58cc6a7e0089a1c3498208897
Brad Dunlap <braddunlapdpt@gmail.com>
2023-01-15 15:55:24 -0500

    feat: tests for translation of braille to text, one character

--- 1mod/projects/night_writer/spec/braille_to_alpha_spec.rb
+++ 1mod/projects/night_writer/spec/braille_to_alpha_spec.rb
@@ -5,9 +5,13 @@ RSpec.describe BrailleToAlpha do
   let(:braille) {BrailleToAlpha.new}
 	input = File.read('./braille.txt')
 	output = File.read('./message.txt')
+
 	describe '#initialize' do
 		it 'exists' do
 			expect(braille).to be_instance_of(BrailleToAlpha)
+		end
+		it 'has attributes' do
+			expect(braille.braille_to_text[["0.", "..", ".."]]).to eq("a")
 		end
 	end
 end
